36c4f924cbf6fc6e88fecaea2ef125b5a02bafd9
Nur A. <nur.almiarravi@gmail.com>
2025-11-04 23:07:05 +0100

    add catalog events

--- client/assets/js/main.js
+++ client/assets/js/main.js
@@ -1,17 +1,19 @@
 const header = document.querySelector('.header__content');
 const logo = header.querySelector('.logo');
 const maxScroll = 150; // на сколько пикселей скролла header плавно поднимается
+const startTop = 40; // начальное смещение сверху в px
 
 window.addEventListener('scroll', () => {
   let scroll = Math.min(window.scrollY, maxScroll);
   let progress = scroll / maxScroll; // от 0 до 1
 
-  // плавно меняем top
-  header.style.top = `${0 * progress}px`; // от 84px до 0px
+  // плавно меняем top от startTop до 0
+  header.style.top = `${startTop * (1 - progress)}px`;
 
   // плавно уменьшаем padding
-  let padding = 1 + (0.4 - 1) * progress; // от 1rem до 0.4rem
-  header.style.padding = `${padding}rem ${1.5 - 0.5 * progress}rem`;
+  let paddingY = 1 + (0.4 - 1) * progress; // от 1rem до 0.4rem
+  let paddingX = 1.5 - 0.5 * progress;      // от 1.5rem до 1rem
+  header.style.padding = `${paddingY}rem ${paddingX}rem`;
 
   // плавно уменьшаем логотип
   let scale = 1 - 0.2 * progress; // от 1 до 0.8
@@ -20,9 +22,3 @@ window.addEventListener('scroll', () => {
   // плавно усиливаем тень
   header.style.boxShadow = `0 ${5 + 3 * progress}px ${10 + 5 * progress}px rgba(0,0,0,${0.2 + 0.1 * progress})`;
 });
-
-
-
-
-
-
