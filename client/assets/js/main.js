b816910de4b866606158cf7acb355e00bb4c4e3e
Nur A. <nur.almiarravi@gmail.com>
2025-11-09 21:28:48 +0100

    add burger menu for phones

--- client/assets/js/main.js
+++ client/assets/js/main.js
@@ -26,6 +26,19 @@ window.addEventListener('scroll', () => {
 
 
 
+// HEADER
+const burger = document.getElementById('burger');
+const nav = document.getElementById('nav');
+
+burger.addEventListener('click', () => {
+  burger.classList.toggle('active');
+  nav.classList.toggle('active');
+});
+// End HERADER
+
+
+
+
 
 // CARD FLIP
 document.querySelectorAll('.card').forEach(card => {
